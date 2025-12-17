2f58e770d47277259a1716cf5539c1e20fb16b42
mukeshmr0728 <158051964+mukeshmr0728@users.noreply.github.com>
2025-12-17 16:01:03 +0530

    Updated sheets-config.ts

--- src/lib/sheets-config.ts
+++ src/lib/sheets-config.ts
@@ -1,3 +1,3 @@
 export const GOOGLE_SHEETS_CONFIG = {
-  SHEET_URL: 'YOUR_GOOGLE_SHEETS_WEB_APP_URL',
+  SHEET_URL: 'https://script.google.com/macros/s/AKfycbygcU5snBM1_Tv0rkgJnZcYAeq0hpFqBoOMN5JtjfoPb6N72tjzUF-tVbDKNDJx1YRk/exec',
 };
